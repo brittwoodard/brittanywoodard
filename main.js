8d0e7a931fff032a63c823e9db6dcacff272099c
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2026-02-28 16:06:13 +0100

    playing with slick slider

--- main.js
+++ main.js
@@ -35,6 +35,29 @@ $('.slider-container').slick({
 	pauseOnHover: false, 
 });
 
+// Portfolio Slider
+ $('.portfolio-container').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: true, // Set to true if you want navigation arrows
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ]
+});
+
 
 // Animate on Scroll
 AOS.init ({
